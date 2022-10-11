ae841b388a141ca36ce3cea972e71132d62f8790
serhii <comc49092@gmail.com>
2022-10-11 14:56:20 +0300

    Refactoring.

--- src/main/java/io/github/lsap/LibraryApplication.java
+++ src/main/java/io/github/lsap/LibraryApplication.java
@@ -15,6 +15,7 @@ import java.util.Scanner;
 class LibraryApplication {
 
     private static final Scanner scanner = new Scanner(System.in);
+    private static final LibraryService libraryService = new LibraryService();
 
     public static void main(String[] args) {
         System.out.println(header());
@@ -23,131 +24,20 @@ class LibraryApplication {
         while (!"exit".equals(input)) {
             input = scanner.nextLine();
             switch (input) {
-                case "1"    -> showAllBooks();
-                case "2"    -> showAllReaders();
-                case "3"    -> addNewReader();
-                case "4"    -> addNewBook();
-                case "5"    -> lendBookToReader();
-                case "6"    -> returnBook();
-                case "7"    -> showAllReaderBooks();
-                case "8"    -> showReaderOfBook();
+                case "1"    -> libraryService.showAllBooks();
+                case "2"    -> libraryService.showAllReaders();
+                case "3"    -> libraryService.addNewReader();
+                case "4"    -> libraryService.addNewBook();
+                case "5"    -> libraryService.lendBookToReader();
+                case "6"    -> libraryService.returnBook();
+                case "7"    -> libraryService.showAllReaderBooks();
+                case "8"    -> libraryService.showReaderOfBook();
                 case "exit" -> { System.out.println("Bye!"); System.exit(0); }
                 default     -> {}
             }
             System.out.println(disclaimer());
         }
 
-    }
-
-    static void showAllBooks() {
-        System.out.println(List.of(Storage.getAllBooks()));
-    }
-
-    static void showAllReaders() {
-        System.out.println(List.of(Storage.getAllReaders()));
-    }
-
-    static void addNewReader() {
-        System.out.println("Please enter new reader's full name!");
-        var readerName = scanner.nextLine();
-        var reader = new Reader();
-        reader.setName(readerName);
-        Storage.addReader(reader);
-    }
-
-    static void addNewBook() {
-        System.out.println(
-                "Please enter new book's title and author separated by “/”. E.g. name/author");
-        var bookInput = scanner.nextLine();
-        var splittedInput = bookInput.split("/");
-        var book = new Book();
-        book.setTitle(splittedInput[0]);
-        book.setAuthor(splittedInput[1]);
-        Storage.addBook(book);
-    }
-
-    static void lendBookToReader() {
-        System.out.println("Please enter bookID and reader's ID separated by “/”. E.g. d/f");
-        var input = scanner.nextLine();
-        var splittedInput = input.split("/");
-        var bookId = Long.parseLong(splittedInput[0]);
-        var readerId = Long.parseLong(splittedInput[1]);
-        try {
-            bookLentToReader(bookId, readerId);
-        } catch (NoEntityWithSuchIdException | NumberFormatException e) {
-            System.err.println(e.getLocalizedMessage());;
-        }
-    }
-
-    static void returnBook() {
-        System.out.println("Please enter book ID");
-        var input = scanner.nextLine();
-        var bookId = Long.parseLong(input);
-        try {
-            returnBook(bookId);
-        } catch (NoEntityWithSuchIdException | NumberFormatException e) {
-            System.err.println(e.getLocalizedMessage());;
-        }
-    }
-
-    private static void returnBook(Long bookId) {
-        if(!Storage.containsBookWithId(bookId)) {
-            throw new NoEntityWithSuchIdException(
-                    "Book with a specified id " + bookId + " does not exist in the storage");
-        }
-        Storage.getReaderIdByBookId(bookId).ifPresentOrElse(
-                readerId -> Storage.returnBookFromReader(bookId, readerId),
-                () -> System.err.println("Can't determine reader ID by book ID " + bookId)
-        );
-    }
-
-    static void showAllReaderBooks() {
-        System.out.println("Please enter reader ID");
-        var input = scanner.nextLine();
-        var readerId = Long.parseLong(input);
-        try {
-            var allBooksOfReader = getAllBooksByReader(readerId);
-            if(allBooksOfReader.isEmpty()) {
-                System.out.println("Reader with ID " + readerId + " has not borrowed books.");
-            } else {
-                allBooksOfReader.forEach(System.out::println);
-            }
-        } catch (NoEntityWithSuchIdException e) {
-            System.err.println(e.getLocalizedMessage());
-        }
-    }
-
-    static void showReaderOfBook() {
-        System.out.println("Please enter book ID");
-        var input = scanner.nextLine();
-        var bookId = Long.parseLong(input);
-        try {
-            showReaderOfBookWithId(bookId);
-        } catch (NoEntityWithSuchIdException e) {
-            System.err.println(e.getLocalizedMessage());
-        }
-    }
-
-    private static void showReaderOfBookWithId(Long bookId) {
-        if(!Storage.containsBookWithId(bookId)) {
-            throw new NoEntityWithSuchIdException(
-                    "Book with a specified id " + bookId + " does not exist in the storage");
-        }
-        Storage.getReaderIdByBookId(bookId)
-                .flatMap(Storage::getBookById)
-                .ifPresentOrElse(
-                        System.out::println,
-                        () -> System.out.println(
-                                "Book with ID " + bookId + " is not borrowed. No reader to show.")
-                );
-    }
-
-    private static List<Long> getAllBooksByReader(Long readerId) {
-        if(!Storage.containsReaderWithId(readerId)) {
-            throw new NoEntityWithSuchIdException(
-                    "Reader with a specified id " + readerId + " does not exist in the storage");
-        }
-        return Storage.getBooksIdsByReaderId(readerId);
     }
 
     static String header() {
@@ -172,18 +62,6 @@ class LibraryApplication {
                
                Type "exit" to stop the program!
                """;
-    }
-
-    private static void bookLentToReader(Long bookId, Long readerId) {
-        if(!Storage.containsReaderWithId(readerId)) {
-            throw new NoEntityWithSuchIdException(
-                    "Reader with a specified id " + readerId + " does not exist in the storage");
-        }
-        if(!Storage.containsBookWithId(bookId)) {
-            throw new NoEntityWithSuchIdException(
-                    "Book with a specified id " + bookId + " does not exist in the storage");
-        }
-        Storage.lendBookToReader(bookId, readerId);
     }
 
 }
