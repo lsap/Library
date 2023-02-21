3877ec90efb8c69648d86e8154218222c7058200
lsap <35534440+lsap@users.noreply.github.com>
2023-02-21 16:50:16 +0200

    Delete NoEntityWithSuchIdException.java

--- src/main/java/io/github/lsap/exception/NoEntityWithSuchIdException.java
+++
@@ -1,9 +0,0 @@
-package io.github.lsap.exception;
-
-public class NoEntityWithSuchIdException extends RuntimeException {
-
-    public NoEntityWithSuchIdException(String message) {
-        super(message);
-    }
-
-}
